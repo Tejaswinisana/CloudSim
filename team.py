4e65ae88654b26ae53ca4281792c93cd1047b46a
sujith5116 <sm5116@columbia.edu>
2024-07-29 23:21:24 -0700

    Req files

---
+++ team.py
@@ -0,0 +1,62 @@
+from telethon import TelegramClient, events
+import logging
+import asyncio
+import re
+
+# Replace these with your credentials
+api_id = '27232480'
+api_hash = 'b00b2f81b37e64a7eca45f11570ed7ac'
+bot_token = '7245992910:AAFjz_Klm6CWR2X2uORIydulKZ8a8WBqswg'
+phone_number = '+17169079367'
+source_group_ids = [ -4198424207 , -1002243936664, -1001371184682, 6145463489, -1002208686405, -1002237728660]
+destination_group_id = -1002208686405
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Define your filter
+def message_passes_filter(message_text):
+    # Check for presence of specific keywords or phrases
+    keywords = ['Oct', 'Nov', 'Dec', 'Available', 'September', 'October', 'Sep', 'November']
+    pattern = re.compile('|'.join(keywords), re.IGNORECASE)
+    return bool(pattern.search(message_text))
+
+async def main():
+    client = TelegramClient('session_name_main', api_id, api_hash)
+    
+    # Connect and sign in
+    await client.start(phone_number)
+
+    @client.on(events.NewMessage(chats= source_group_ids))
+    async def handler(event):
+        message = event.message
+        chat = await client.get_entity(message.chat_id)
+        message_id = message.id
+        message_text = message.text or ""
+
+        if message.media:
+            # Construct the link to the media message
+            user_name = chat.username
+            message_id = message.id
+            message_url = f"https://t.me/{chat.username}/{message_id}"
+
+            # Send the link to the private chat
+            try:
+                await client.send_message(destination_group_id, f"Screen Shot link: \n {message_url}")
+                logging.info(f"Message URL: {message_url}")
+            except Exception as e:
+                logging.error(f"An error occurred while sending the media link: {e}")
+        elif message_passes_filter(message_text):
+            # If it's a text message and passes the filter
+            try:
+                await client.send_message(destination_group_id, message_text)
+                logging.info(f"Message sent: {message_text}")
+            except Exception as e:
+                logging.error(f"An error occurred while sending the message: {e}")
+        else:
+            logging.info(f"Message did not pass filter: {message_text}")
+
+    await client.run_until_disconnected()
+
+if __name__ == '__main__':
+    asyncio.run(main())
